4e7e5c18cccda652f343ef56dd39e8a0e9aaa113
startmktbr <>
2022-10-23 11:58:12 -0300

    added responsive image

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 'use strict'
 
-class CanvasFlyer {
+/**
+ * Represents a flyer application that uses canvas.
+ */
+class CanvasFlyerApp {
   #nameInputElement;
   #downloadButtonElement;
   #canvasContainerElement;
@@ -8,11 +11,15 @@ class CanvasFlyer {
   #imageSource;
 
   /**
-   * @param {HTMLInputElement} nameInputElement
-   * @param {HTMLButtonElement} downloadButtonElement
-   * @param {string} imageSource
-   * @param {HTMLCanvasElement} canvasElement
-   * @param {HTMLDivElement} canvasContainerElement
+   * Creates a new instance of a `CanvasFlyerApp`.
+   * @param {HTMLInputElement} nameInputElement The input element of type text
+   * that contains the person's name.
+   * @param {HTMLButtonElement} downloadButtonElement The button element that
+   * downloads the image.
+   * @param {string} imageSource The source of the image.
+   * @param {HTMLCanvasElement} canvasElement The canvas element.
+   * @param {HTMLDivElement} canvasContainerElement The container of the canvas
+   * element.
    */
   constructor(
       nameInputElement,
@@ -28,27 +35,58 @@ class CanvasFlyer {
     this.#canvasContainerElement = canvasContainerElement;
 
     window.addEventListener('load', () => {
-      this.resizeCanvasElement();
+      this.drawCanvasElement();
     });
 
     window.addEventListener('resize', () => {
-      this.resizeCanvasElement();
+      this.drawCanvasElement();
     });
   }
 
+  /**
+   * Creates the canvas element and draw with its contents.
+   */
+  drawCanvasElement() {
+    this.resizeCanvasElement();
+    this.drawImage();
+  }
+
+  /**
+   * Recreate the canvas element to fit inside its container.
+   */
   resizeCanvasElement() {
     this.#canvasElement.width = this.#canvasContainerElement.offsetWidth;
-    this.#canvasElement.height = this.#canvasContainerElement.offsetHeight;
+    this.#canvasElement.height = this.#canvasContainerElement.offsetWidth;
+  }
+
+  /**
+   * Draws the image in the canvas element.
+   */
+  drawImage() {
+    const canvasContext = this.#canvasElement.getContext('2d');
+
+    const imageElement = new Image();
+    imageElement.src = this.#imageSource;
+
+    imageElement.addEventListener('load', () => {
+      canvasContext.drawImage(
+          imageElement,
+          0,
+          0,
+          this.#canvasElement.width,
+          this.#canvasElement.height,
+      );
+    });
   }
 }
 
 const nameInputElement = document.querySelector('.js-name-form-input');
 const downloadButtonElement = document.querySelector('.js-name-form-button');
-const canvasContainerElement = document.querySelector('.js-container');
-const canvasElement = document.querySelector('.js-canvas');
 const imageSource = 'flyer-base.jpg';
+const canvasElement = document.querySelector('.js-canvas');
+const canvasContainerElement = document.querySelector('.js-container');
 
-const canvasFlyer = new CanvasFlyer(
+const canvasFlyer = new CanvasFlyerApp(
     nameInputElement,
     downloadButtonElement,
     imageSource,
